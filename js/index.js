4e54afae7b74c394e6530e05e8925e81ce66a8b3
Vaibhav Bisht <vaibhavbisht@Vaibhavs-MacBook-Air.local>
2020-02-18 19:29:06 +0530

    OriInitial Commit

---
+++ js/index.js
@@ -0,0 +1,345 @@
+let emailValue = '';
+let emailValidity = false;
+let FCMToken = '';
+let recaptchaToken = '';
+let messaging = firebase.messaging();
+
+messaging.onMessage(function(payload){
+    return self.ServiceWorkerRegistration.showNotification(title,options);
+})
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function updateEmailToken(){
+    fetch('https://dscrec19.herokuapp.com/notifs/?email='+emailValue+'&device_id='+FCMToken, {
+            method:'PUT',
+            crossDomain:true,
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJSON){     
+            if(responseJSON.status === false){
+                $('.error-message').addClass('message-active');
+                document.getElementById('error-message').innerHTML = "Error Changing Device";
+                setTimeout(function(){ 
+                    $('.error-message').removeClass('message-active');
+                    document.getElementById('error-message').innerHTML = null;
+                }, 2500);
+            }
+            else{
+                $('.success-message').addClass('message-active');
+                document.getElementById('success-message').innerHTML = "New Device Added";
+                setTimeout(function(){ 
+                    $('.success-message').removeClass('message-active');
+                    document.getElementById('success-message').innerHTML = null;
+                }, 2500);
+            }
+        })
+
+}
+
+function submitEmailwithToken(){
+
+    fetch('https://dscrec19.herokuapp.com/notifs/', {
+            method:'POST',
+            crossDomain:true,
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                emailAddress: emailValue,
+                deviceID: FCMToken
+            })
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJSON){
+            if(responseJSON.err === undefined){
+                $('.success-message').addClass('message-active');
+                document.getElementById('success-message').innerHTML = "Welcome to the Notification Gang!";
+                setTimeout(function(){ 
+                    $('.success-message').removeClass('message-active');
+                    document.getElementById('success-message').innerHTML = null;
+                }, 2500);
+            }
+            else if(responseJSON.err === 'User already registered'){
+                $('.change-device-message').addClass('change-device-active');
+            }
+            else{
+                $('.error-message').addClass('message-active');
+                document.getElementById('error-message').innerHTML = "Error Registering your Email";
+                setTimeout(function(){ 
+                    $('.error-message').removeClass('message-active');
+                    document.getElementById('error-message').innerHTML = null;
+                }, 2500);
+            }
+        })
+    
+}
+
+
+function submitEmail(){
+    
+    if(emailValidity){
+        messaging.requestPermission()
+        .then((permission) => {
+            console.log('Notification permission granted.');
+            return messaging.getToken();
+        })
+        .then((token) => {
+            FCMToken = token;
+            submitEmailwithToken();
+            localStorage.setItem('userNotificationTokenDSC', FCMToken);
+        });
+    }
+    else{
+        console.log('Invalid Email')
+    }
+}
+
+if(localStorage.getItem('userNotificationTokenDSC')===null){
+    FCMToken = generateToken();
+    localStorage.setItem('userNotificationTokenDSC', FCMToken);
+}else{
+    FCMToken = localStorage.getItem('userNotificationTokenDSC');
+}
+
+function generateToken() {
+    var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var token = '';
+    for(var i = 0; i < 16; i++) {
+        token += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return token;
+}
+
+$(document).ready(function () {
+
+    $('#change-deviceID').click(function(){
+        updateEmailToken();
+        $('.change-device-message').removeClass('change-device-active');
+    })
+
+    $('#no-change-deviceID').click(function(){
+        $('.change-device-message').removeClass('change-device-active');
+    })
+
+
+    $("#get-updates").click((e) => {
+
+        submitEmail();
+            
+        // grecaptcha.ready(() => {
+
+            
+
+        //         // grecaptcha.execute('6LdiJ8QUAAAAAFCiBqwvhGOI2Ho3v-EFD73PAiBn', { action: '/' }).then(async (token) => {
+
+        //         //     let body = {
+        //         //         "g-recaptcha-response": token, "email": emailValue
+        //         //     }
+
+        //         //     console.table(body)
+        //         //     console.log(token)
+        //         //     recaptchaToken = token;
+
+        //         //     let resp = await fetch("/updates", {
+        //         //         method: "POST",
+        //         //         headers: {
+        //         //             'Accept': 'application/json',
+        //         //             'Content-Type': 'application/json'
+        //         //         },
+        //         //         body: JSON.stringify(body)
+        //         //     })
+
+        //         //     submitEmail();
+                    
+        //         //     let reply = await resp.json()
+
+
+        //         //     if (reply.status === false) {
+        //         //         alert("Captcha Not Verified")
+        //         //     } else if (reply.err != null) {
+        //         //         console.log(reply.err)
+        //         //         if (reply.err === "Already responded")
+        //         //             alert(reply.err)
+        //         //     } else {
+
+        //         //         console.log(reply)
+
+        //         //     }
+
+        //         // });
+        // });
+
+    });
+
+    document.getElementById('email').addEventListener("input", function() {
+        emailValue = $("#email").val();
+
+        if(validateEmail(emailValue)){
+            $('#email').addClass('text-field-true');
+            $('#email').removeClass('text-field-false');
+            emailValidity = true;
+        }
+        else{
+            $('#email').removeClass('text-field-true');
+            $('#email').addClass('text-field-false');
+            emailValidity = false;
+        }
+        
+    });
+
+    checkDark();
+
+    $('#dark-light-toggle').click(function(){
+        toggleDark();
+        checkDark();
+    })
+
+    // Scroll Clicks
+    $("#home").click(function () {
+        $('.main-menu').fadeOut(200)
+        $('html, body').animate({
+            scrollTop: $("#home-scroll").offset().top
+        }, 600);
+    });
+
+    $(".navbar-dsc-logo").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#home-scroll").offset().top
+        }, 600);
+    });
+
+    $("#home-desk").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#home-scroll").offset().top
+        }, 600);
+    });
+
+    $("#down-arrow").click(function () {
+        $('.main-menu').fadeOut(200)
+        $('html, body').animate({
+            scrollTop: $("#our-work-scroll").offset().top
+        }, 900);
+    })
+
+    $("#our-work").click(function () {
+        $('.main-menu').fadeOut(200)
+        $('html, body').animate({
+            scrollTop: $("#our-work-scroll").offset().top
+        }, 900);
+
+    });
+
+    $("#our-work-desk").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#our-work-scroll").offset().top
+        }, 900);
+
+    });
+
+    $("#team").click(function () {
+        $('.main-menu').fadeOut(200)
+        $('html, body').animate({
+            scrollTop: $("#team-scroll").offset().top
+        }, 1200);
+
+    });
+
+    $("#team-desk").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#team-scroll").offset().top
+        }, 1200);
+
+    });
+
+    $("#updates").click(function () {
+        $('.main-menu').fadeOut(200)
+        $('html, body').animate({
+            scrollTop: $("#updates-scroll").offset().top
+        }, 1800);
+
+    });
+
+    $("#updates-desk").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#updates-scroll").offset().top
+        }, 1800);
+
+    });
+
+    $("#contact").click(function () {
+        $('.main-menu').fadeOut(200)
+        $('html, body').animate({
+            scrollTop: $("#contact-scroll").offset().top
+        }, 2100);
+
+    });
+
+    $("#contact-desk").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#contact-scroll").offset().top
+        }, 2100);
+
+    });
+
+    // Open Menu
+    $('#menu-open').click(function () {
+        $('.main-menu').fadeIn(100);
+    })
+
+    // Close Menu
+    $('#menu-close').click(function () {
+        $('.main-menu').fadeOut(100);
+    })
+
+    $(window).on('scroll', function () {
+
+        var LogoMark = $(".main-text-holder-heading").offset().top;
+
+        if ($(this).scrollTop() > LogoMark) {
+            if (!$('.navbar').hasClass('navbar-scrolled')) {
+                $('.navbar').addClass('navbar-scrolled');
+            }
+        } else {
+            if ($('.navbar').hasClass('navbar-scrolled')) {
+                $('.navbar').removeClass('navbar-scrolled');
+            }
+        }
+    });
+})
+
+var toggleDark = function(){
+    if(!$('body').hasClass('dark')){
+        localStorage.setItem('dark',true);
+    }
+    else{
+        localStorage.setItem('dark',false);
+    }
+}
+
+var checkDark = function (){
+
+    var dark = localStorage.getItem('dark');
+
+    if(dark==='true'){
+        $('body').addClass('dark');
+        $('.dark-light-toggle').children().text('I want light mode');
+    }
+    else{
+        $('body').removeClass('dark');
+        $('.dark-light-toggle').children().text('I want dark mode');
+    }
+
+}
---
+++ sw.js
@@ -0,0 +1,94 @@
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/team.html',
+  './css/index.css',
+  './css/team.css',
+  './css/svg.css',
+  './js/team.js',
+  './js/index.js',
+  './images/after-landing.svg',
+  './images/after-landing-dark.svg',
+  './images/Behance.svg',
+  './images/dsc-logo-square.svg',
+  './images/Facebook.svg',
+  './images/Github.svg',
+  './images/Instagram.svg',
+  './images/landing.svg',
+  './images/Linkedin.svg',
+  './images/Medium.svg',
+  './images/Twitter.svg',
+  './images/Youtube.svg',
+  './images/team.svg',
+  './images/board19/samarth.jpg',
+  './images/board19/ayush.jpg',
+  './images/board19/samyak.jpg',
+  './images/board19/aritro.jpg',
+  './images/board19/dhiraj.jpg',
+  './images/board19/jayakrishna.jpg',
+  './images/board19/abhishek.jpg',
+  './images/board19/raina.jpg',
+  './images/board19/apurva.jpg',
+  './images/board19/preethi.jpg',
+  './images/board19/amrut.jpg',
+  './images/techteam/abhishek.jpg',
+  './images/techteam/amogh.jpg',
+  './images/techteam/ananya.jpg',
+  './images/techteam/angad.jpg',
+  './images/techteam/deepak.jpg',
+  './images/techteam/nikhil.jpg',
+  './images/techteam/paritosh.jpg',
+  './images/techteam/prateek.jpg',
+  './images/techteam/riddhi.jpg',
+  './images/techteam/satkriti.jpg',
+  './images/techteam/ubaid.jpg',
+  './images/techteam/vikrame.jpg',
+  './images/techteam/vishaal.jpg',
+  './images/techteam/yaswant.jpg',
+  './images/techteam/purushottam.jpg',
+  './images/managers/akshat.jpg',
+  './images/managers/arjun.jpg',
+  './images/managers/cyril.jpg',
+  './images/managers/deepak.jpg',
+  './images/managers/dhruv.jpg',
+  './images/managers/hardik.jpg',
+  './images/managers/kritika.jpg',
+  './images/managers/manorama.jpg',
+  './images/managers/meherdeep.jpg',
+  './images/managers/muskan.jpg',
+  './images/managers/naynika.jpg',
+  './images/managers/saloni.jpg',
+  './images/managers/sanchi.jpg',
+  './images/managers/shreya.jpg',
+  './images/managers/shubham.jpg',
+  './images/managers/vivek.jpg',
+  './images/designers/ekaansh.jpg',
+  './images/designers/aditya.jpg',
+  './images/designers/riya.jpg',
+  './images/designers/ruchica.jpg',
+  './images/designers/shravani.jpg',
+  './images/designers/ronish.jpg',
+  './icons/icon-512x512.png',
+  './icons/favicons/favicon.ico',
+	'./images/notification_icons/recruitments_blog.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open('dscvit').then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })                                                                                                                                                                                                                    
+  );
+});
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
